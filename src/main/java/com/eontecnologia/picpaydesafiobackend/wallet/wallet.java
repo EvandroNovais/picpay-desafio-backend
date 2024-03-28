21d3e267f43d1f9239d94bc40115cb7e5f35a64c
Evandro Oliveira de Novais <evandro-novais@outlook.com>
2024-03-28 11:27:55 -0300

    Create services and repositories

--- src/main/java/com/eontecnologia/picpaydesafiobackend/wallet/wallet.java
+++ src/main/java/com/eontecnologia/picpaydesafiobackend/wallet/wallet.java
@@ -4,13 +4,13 @@ import java.math.BigDecimal;
 
 import org.springframework.data.annotation.Id;
 
-public record wallet(
-    @Id Long id,
-    String fullName,
-    Long cpf,
-    String email,
-    String password,
-    int type,
-    BigDecimal balance) {
+public record Wallet(
+        @Id Long id,
+        String fullName,
+        Long cpf,
+        String email,
+        String password,
+        int type,
+        BigDecimal balance) {
 
 }
