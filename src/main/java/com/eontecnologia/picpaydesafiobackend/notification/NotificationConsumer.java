0b52b86ffe6abb1de20e82ff7b5a15bfc7739c28
Evandro Oliveira de Novais <evandro-novais@outlook.com>
2024-04-04 08:29:05 -0300

    create notification consumer

--- src/main/java/com/eontecnologia/picpaydesafiobackend/notification/NotificationConsumer.java
+++ src/main/java/com/eontecnologia/picpaydesafiobackend/notification/NotificationConsumer.java
@@ -1,5 +1,16 @@
 package com.eontecnologia.picpaydesafiobackend.notification;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
 public class NotificationConsumer {
 
+  private RestClient restClient;
+
+  public NotificationConsumer(RestClient.Builder builder) {
+    this.restClient = builder
+        .baseUrl("null")
+        .build();
+  }
 }
