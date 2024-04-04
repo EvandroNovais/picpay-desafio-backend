0fb047db22e66a3128a86e1ee5d4d7b22eb85a68
Evandro Oliveira de Novais <evandro-novais@outlook.com>
2024-04-04 09:33:55 -0300

    Revert "implement notificationi"
    This reverts commit 61577ee5836367edfd51e62166d9911529bee715.

--- src/main/java/com/eontecnologia/picpaydesafiobackend/notification/NotificationException.java
+++
@@ -1,7 +0,0 @@
-package com.eontecnologia.picpaydesafiobackend.notification;
-
-public class NotificationException extends RuntimeException {
-  public NotificationException(String message) {
-    super(message);
-  }
-}
