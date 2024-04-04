61577ee5836367edfd51e62166d9911529bee715
Evandro Oliveira de Novais <evandro-novais@outlook.com>
2024-04-04 09:32:37 -0300

    implement notificationi

--- src/main/java/com/eontecnologia/picpaydesafiobackend/notification/NotificationConsumer.java
+++ src/main/java/com/eontecnologia/picpaydesafiobackend/notification/NotificationConsumer.java
@@ -1,7 +1,10 @@
 package com.eontecnologia.picpaydesafiobackend.notification;
 
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+
+import com.eontecnologia.picpaydesafiobackend.transaction.Transaction;
 
 @Service
 public class NotificationConsumer {
@@ -12,5 +15,19 @@ public class NotificationConsumer {
     this.restClient = builder
         .baseUrl("null")
         .build();
+  }
+
+  @KafkaListener(topics = "transaction-notification", groupId = "picpay-desafio-backend")
+  public void receiveNotification(Transaction transaction) {
+
+    var response = restClient.get()
+        .retrieve()
+        .toEntity(Notification.class);
+
+    var body = response.getBody();
+
+    if (response.getStatusCode().isError() || body != null && !body.message()) {
+      throw new NotificationException("Error sending notification");
+    }
   }
 }
